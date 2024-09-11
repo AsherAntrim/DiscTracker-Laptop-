56028a2b03d2c0a9f285b035beb0d692b25aa8eb
Asher <aantrim246@cedarville.edu>
2024-09-11 14:04:29 -0400

    Initial Commit

---
+++ DiscTracker/DiscTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscTrackerApp.swift
+//  DiscTracker
+//
+//  Created by Asher Antrim on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiscTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscTrackerTests/DiscTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscTrackerTests.swift
+//  DiscTrackerTests
+//
+//  Created by Asher Antrim on 9/11/24.
+//
+
+import Testing
+@testable import DiscTracker
+
+struct DiscTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
